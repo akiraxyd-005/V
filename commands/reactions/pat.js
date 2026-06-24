8ccebcee630a204a1697e24a9db00001e7946278
🌸✿𝓐𝓶𝓪𝓲-𝓬𝓱𝓪𝓷✿🌸 <akiraxyd@gmail.com>
2026-06-24 13:14:58 +0300

    Create pat.js

---
+++ commands/reactions/pat.js
@@ -0,0 +1,23 @@
+const Anime_Images = require('anime-images-api');
+const API = new Anime_Images();
+
+module.exports = {
+    name: 'pat',
+    category: 'reactions',
+    description: 'Pat someone',
+    usage: '§pat @user',
+    async execute(sock, msg, args, extra) {
+        let target = 'someone';
+        if (msg.message.extendedTextMessage?.contextInfo?.mentionedJid?.length) {
+            target = msg.message.extendedTextMessage.contextInfo.mentionedJid[0];
+        }
+        
+        const { image } = await API.sfw.pat();
+        
+        await sock.sendMessage(msg.key.remoteJid, {
+            image: { url: image },
+            caption: `🫳 *${msg.pushName || 'Someone'}* patted *@${target.split('@')[0]}*!`,
+            mentions: [target]
+        });
+    }
+};
